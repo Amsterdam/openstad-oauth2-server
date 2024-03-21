07c3b60c0dd0cd1329fc7a95937a2925d80dd559
IanR01 <ian.r.89@gmail.com>
2024-03-21 13:20:29 +0100

    '.includes' instead of '.indexOf'

--- reset.js
+++ reset.js
@@ -5,7 +5,7 @@ const constants = require('fs').constants;
 const db = require('./db');
 
 // check for force flag '-f' on commandline to force sync, like: node reset.js -f
-const force = process.argv.indexOf('-f') > -1
+const force = process.argv.includes('-f')
 
 async function doReset() {
 
