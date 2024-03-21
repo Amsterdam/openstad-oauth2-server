8093dc55595865210da481940def663486878cf7
IanR01 <IanR01@users.noreply.github.com>
2024-03-21 13:22:27 +0100

    Remove auto forced sync (#17)
    * add check for -f flag
    
    * '.includes' instead of '.indexOf'

--- reset.js
+++ reset.js
@@ -4,13 +4,16 @@ const fs = require('fs').promises;
 const constants = require('fs').constants;
 const db = require('./db');
 
+// check for force flag '-f' on commandline to force sync, like: node reset.js -f
+const force = process.argv.includes('-f')
+
 async function doReset() {
 
   try {
 
     console.log('Syncing...');
 
-    await db.sequelize.sync({force: true})
+    await db.sequelize.sync({force})
 
     console.log('Adding default data...');
     let datafile = process.env.NODE_ENV || 'default';
