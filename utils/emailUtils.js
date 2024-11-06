d3479aadac37c7c4ea203b73c56fbee155cf950c
ferhany2002 <ferhan.y2002@gmail.com>
2024-11-06 13:02:36 +0100

    Only send emails from whitelisted addresses

---
+++ utils/emailUtils.js
@@ -0,0 +1,9 @@
+const whitelistedEmails = process.env.WHITELISTED_EMAILS.split('\n').slice(0, -1);
+
+function isWhitelistedEmail(from) {
+  return whitelistedEmails.some(whitelistedEmail => from.includes(whitelistedEmail));
+}
+
+module.exports = {
+  isWhitelistedEmail
+};
