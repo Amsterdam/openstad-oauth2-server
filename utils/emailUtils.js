3daf4fab90db0817dbe1c91938a1c24d1183ebdf
ferhany2002 <ferhan.y2002@gmail.com>
2024-11-06 13:30:54 +0100

    Add fallback if env variables aren't set

--- utils/emailUtils.js
+++ utils/emailUtils.js
@@ -1,7 +1,9 @@
-const whitelistedEmails = process.env.WHITELISTED_EMAILS.split('\n').slice(0, -1);
+const whitelistedEmails = process.env.WHITELISTED_EMAILS 
+  ? process.env.WHITELISTED_EMAILS.split('\n').slice(0, -1) 
+  : [];
 
 function isWhitelistedEmail(from) {
-  return whitelistedEmails.some(whitelistedEmail => from.includes(whitelistedEmail));
+  return whitelistedEmails.length > 0 && whitelistedEmails.some(whitelistedEmail => from.includes(whitelistedEmail));
 }
 
 module.exports = {
